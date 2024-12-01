dffb655b2e7cc6263ee4d64e400d480b8939b86e
shugganait <daniarm209@gmail.com>
2024-12-01 23:39:50 +0600

    Begininning

---
+++ app/src/main/java/by/shug/interntrack/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,29 @@
+package by.shug.interntrack.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import by.shug.interntrack.databinding.FragmentDashboardBinding;
+
+public class DashboardFragment extends Fragment {
+
+    private FragmentDashboardBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
